fd2def024ac114082861b813097e6e0b9f0bd886
SketchingDev <SketchingDev@users.noreply.github.com>
2017-11-21 22:28:05 +0000

    Pass same messages to each handler
    Only poll for new messages once passing all existing
    messages to each handler

--- doodledashboard/lucas/dashboard.py
+++ doodledashboard/lucas/dashboard.py
@@ -1,22 +1,29 @@
 import time
 
 import itertools
+import logging
 
 
 class Dashboard:
     def __init__(self, slack_config, display):
         self._slack_config = slack_config
         self._display = display
+        self._logger = logging.getLogger('raspberry_pi_dashboard.Dashboard')
 
     def start(self):
         repository = self.get_repository(self._slack_config)
 
-        update_interval = self.get_update_interval()
-        for handler in itertools.cycle(self.get_handlers()):
-            messages = repository.get_latest_messages()
-            messages = Dashboard._filter_messages_containing_text(messages, handler.get_tag())
+        handlers = self.get_handlers()
+        self._logger.info('%s handlers loaded' % len(handlers))
 
-            handler.draw(self._display, messages)
+        messages = []
+        update_interval = self.get_update_interval()
+        for handler in itertools.cycle(handlers):
+            if handlers[0] is handler:
+                messages = repository.get_latest_messages()
+
+            handler_messages = Dashboard._filter_messages_containing_text(messages, handler.get_tag())
+            handler.draw(self._display, handler_messages)
 
             time.sleep(update_interval)
 

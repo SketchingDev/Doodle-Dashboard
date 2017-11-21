35e4eea39d99f6cfd2c53df05e2d2561fc4e7dcc
SketchingDev <SketchingDev@users.noreply.github.com>
2017-11-21 22:26:39 +0000

    Handle intermittent connection failures

--- doodledashboard/lucas/repositories.py
+++ doodledashboard/lucas/repositories.py
@@ -1,5 +1,7 @@
 import logging
 
+from requests.exceptions import ConnectionError
+
 
 class MessageModel:
     def __init__(self, text):
@@ -18,43 +20,68 @@ class Repository:
 
 
 class SlackRepository(Repository):
-    channel = None
+    _channel = None
 
     def __init__(self, client, channel_name):
         Repository.__init__(self)
         self._client = client
         self._channel_name = channel_name
         self._logger = logging.getLogger('raspberry_pi_dashboard.SlackRepository')
+        self._connected = False
         self._connected_previously = False
 
-    def _connect(self):
-        return self._client.rtm_connect(with_team_state=False)
-
     def get_latest_messages(self):
-        if not self._connect():
+        if not self._connected:
+            self._connected = self._try_connect()
+
+        if not self._channel:
+            self._channel = self._try_find_channel(self._channel_name)
+
+        if not self._test_connection():
+            self._logger.info('Failed to connect to Slack, will try again next time around')
+            self._connected = False
+            return []
+
+        events = self._client.rtm_read()
+        events = SlackRepository._filter_events_by_channel(self._channel, events)
+        events = SlackRepository._filter_events_by_type(events, 'message')
+        events = SlackRepository._filter_events_with_text(events)
+
+        return [MessageModel(event['text']) for event in events]
+
+    def _test_connection(self):
+        try:
+            self._client.api_call("api.test")
+            return True
+        except ConnectionError:
+            return False
+
+    def _try_connect(self):
+        connected = self._client.rtm_connect(with_team_state=False)
+        if connected:
+            self._logger.info('Connected to Slack. Huzzah!')
+            self._connected_previously = True
+        else:
             if self._connected_previously:
                 message = 'Failed to connect to Slack. I\'ve connected before so likely the internet is just down.'
             else:
                 message = 'Failed to connect to Slack. Is the Slack token correct?'
 
             self._logger.info(message)
-            return []
-        else:
-            self._connected_previously = True
 
-            if not self.channel:
-                self.channel = self._find_channel(self._channel_name)
+        return connected
 
-                if not self.channel:
-                    self._logger.info("Failed to find Slack channel '%s'. Have you provided created it?" % self._channel_name)
+    def _try_find_channel(self, channel_name):
+        channel = None
+        try:
+            channel = self._find_channel(channel_name)
+            if not channel:
+                self._logger.info(
+                    "Failed to find Slack channel '%s'. Have you provided created it?" % self._channel_name)
+        except ConnectionError:
+            pass
 
-            events = self._client.rtm_read()
-            logging.info(events)
-            events = SlackRepository._filter_events_by_channel(self.channel, events)
-            events = SlackRepository._filter_events_by_type(events, 'message')
-            events = SlackRepository._filter_events_with_text(events)
-
-            return [MessageModel(event['text']) for event in events]
+        return channel
 
     def _find_channel(self, channel_name):
         channel_list = self._client.api_call("channels.list", exclude_archived=1)

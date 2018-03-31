dcc35b329b138a4cf88816b52f937d2f431d2399
SketchingDev <SketchingDev@users.noreply.github.com>
2018-03-31 18:04:17 +0100

    Update RSS feed in flaky test

--- tests/doodledashboard/datasources/test_rss_it.py
+++ tests/doodledashboard/datasources/test_rss_it.py
@@ -6,7 +6,7 @@ from doodledashboard.datasources.rss import RssFeed
 class TestRssFeedIT(unittest.TestCase):
 
     def test_real_rss_feed_parsed(self):
-        repo = RssFeed('http://open.live.bbc.co.uk/weather/feeds/en/2643743/3dayforecast.rss')
+        repo = RssFeed('https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123')
         messages = repo.get_latest_messages()
 
         self.assertTrue(len(messages) > 0)

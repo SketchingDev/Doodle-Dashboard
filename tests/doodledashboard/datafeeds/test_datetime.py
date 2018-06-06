d036f94d8e17e09427bcf5dfb3f9699c32a92953
SketchingDev <SketchingDev@users.noreply.github.com>
2018-06-06 12:56:08 +0100

    Refactor tests datetime datafeed tests to use dictionary

--- tests/doodledashboard/datafeeds/test_datetime.py
+++ tests/doodledashboard/datafeeds/test_datetime.py
@@ -1,31 +1,41 @@
 import unittest
-from doodledashboard.configuration.config import DashboardConfigReader
+
 from doodledashboard.datafeeds.datetime import DateTimeFeedSection, DateTimeFeed
 
 
 class TestCliStart(unittest.TestCase):
-    _YAML_CONFIG = """
-        data-feeds:
-          - source: datetime
-    """
+
+    def test_section_creates_for_datetime(self):
+        config = {
+            "source": "datetime"
+        }
+        self.assertTrue(DateTimeFeedSection().can_create(config))
+
+    def test_section_does_not_create_for_other(self):
+        config = {
+            "source": "other"
+        }
+        self.assertFalse(DateTimeFeedSection().can_create(config))
 
     def test_feed_is_created_from_configuration(self):
-        config_reader = DashboardConfigReader()
-        config_reader.add_data_feed_creators([DateTimeFeedSection()])
+        config = {
+            "source": "datetime"
+        }
 
-        dashboard = config_reader.read_yaml(TestCliStart._YAML_CONFIG)
+        data_feed = DateTimeFeedSection().create(config)
 
-        data_feeds = dashboard.get_data_feeds()
-        self.assertEqual(1, len(data_feeds))
-        self.assertIsInstance(data_feeds[0], DateTimeFeed)
+        self.assertIsInstance(data_feed, DateTimeFeed)
 
     def test_feed_returns_date_and_time(self):
-        entities = DateTimeFeed().get_latest_entities()
+        config = {
+            "source": "datetime"
+        }
 
-        self.assertEqual(1, len(entities), "Returns one textual entity per poll")
+        data_feed = DateTimeFeedSection().create(config)
+        entities = data_feed.get_latest_entities()
 
-        entity = entities[0]
-        self.assertRegex(entity.get_text(), "\d{4}-\d{2}-\d{2} \d{2}:\d{2}", "Textual data matches date/time pattern")
+        self.assertEqual(1, len(entities))
+        self.assertRegex(entities[0].get_text(), "\d{4}-\d{2}-\d{2} \d{2}:\d{2}", "Text matches date/time pattern")
 
 
 if __name__ == "__main__":
